261ee95fc5b153b9d807dc8f56383a8cf4af00ad
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-23 08:30:48 -0700

    Fix autocomplete error in test[schedule] declaration

--- __tests__/ageCalc.test.js
+++ __tests__/ageCalc.test.js
@@ -1,7 +1,7 @@
 import AgeCalc from '../src/js/ageCalc.js';
 
 describe('AgeCalc', () => {
-    TestSchedule('Should correctly create an ageCalc object with earthAge property', () => {
+    Test('Should correctly create an ageCalc object with earthAge property', () => {
         const ageCalc = new AgeCalc(25);
         expect(ageCalc.earthAge).toEqual(25);
     });
