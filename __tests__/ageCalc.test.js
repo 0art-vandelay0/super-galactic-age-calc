bd11d44dc9c4f24b22a3d331023f9284e66887fd
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-23 08:48:42 -0700

    Add AgeCalc test 2 w/o added functionality

--- __tests__/ageCalc.test.js
+++ __tests__/ageCalc.test.js
@@ -5,4 +5,14 @@ describe('AgeCalc', () => {
         const ageCalc = new AgeCalc(25);
         expect(ageCalc.earthAge).toEqual(25);
     });
+
+    test('Should correctly create an ageCalc object with solarPlanetYears property', () => {
+        const ageCalc = new AgeCalc(25);
+        expect(ageCalc.solarYears).toEqual({
+            mercury: 0.24,
+            venus: 0.62,
+            mars: 1.88,
+            jupiter: 11.86
+        });
+    });
 });
