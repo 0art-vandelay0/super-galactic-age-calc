9b6fedbaf17f337a7dc9d55fbbf56d117abf39ef
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-23 11:05:31 -0700

    Remove original BirthdayYears constructor test to hold different properties

--- __tests__/birthdayYears.test.js
+++ __tests__/birthdayYears.test.js
@@ -1,9 +1,19 @@
 import BirthdayYears from '../src/js/birthdayYears.js';
+import AgeCalc from '../src/js/ageCalc.js';
 
 describe('BirthdayYears', () => {
-
-    test('Should create a birthdayYears object with earthAge property', () => {
-        const birthdayYears = new BirthdayYears(25);
-        expect(birthdayYears.earthAge).toEqual(25);
+    test('Should create a birthdayYears object with mercuryYear, venusYear, marsYear, jupiterYear, pastBirthday, and futureBirthday properties', () => {
+        const ageCalc = new AgeCalc(25);
+        const pastBirthday = 20;
+        const futureBirthday = 30;
+        const birthdayYears = new BirthdayYears(ageCalc, pastBirthday, futureBirthday);
+    
+        expect(birthdayYears.mercuryYear).toEqual(ageCalc.solarPlanetYears.mercury);
+        expect(birthdayYears.venusYear).toEqual(ageCalc.solarPlanetYears.venus);
+        expect(birthdayYears.marsYear).toEqual(ageCalc.solarPlanetYears.mars);
+        expect(birthdayYears.jupiterYear).toEqual(ageCalc.solarPlanetYears.jupiter);
+        expect(birthdayYears.pastBirthday).toEqual(pastBirthday);
+        expect(birthdayYears.futureBirthday).toEqual(futureBirthday);
     });
+
 });
--- src/js/birthdayYears.js
+++ src/js/birthdayYears.js
@@ -1,8 +1,1 @@
 import AgeCalc from './ageCalc.js';
-
-export default class BirthdayYears {
-    constructor(ageCalc) {
-        this.ageCalc = ageCalc;
-    }
-}
-    
