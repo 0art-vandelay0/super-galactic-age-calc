38d5f3a87c5bee7b611d96536ca68709879b8641
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-23 10:19:07 -0700

    Add new ageCalc instance in beforeEach declaration to minimize each test's code

--- __tests__/ageCalc.test.js
+++ __tests__/ageCalc.test.js
@@ -1,13 +1,20 @@
 import AgeCalc from '../src/js/ageCalc.js';
 
 describe('AgeCalc', () => {
+
+    let ageCalc;
+
     test('Should correctly create an ageCalc object with earthAge property', () => {
-        const ageCalc = new AgeCalc(25);
+        // const ageCalc = new AgeCalc(25);
         expect(ageCalc.earthAge).toEqual(25);
     });
 
+    beforeEach(() => {
+        ageCalc = new AgeCalc(25);
+    });
+
     test('Should correctly create an ageCalc object with solarPlanetYears property', () => {
-        const ageCalc = new AgeCalc(25);
+        // const ageCalc = new AgeCalc(25);
         expect(ageCalc.solarPlanetYears).toEqual({
             mercury: 0.24,
             venus: 0.62,
@@ -17,22 +24,22 @@ describe('AgeCalc', () => {
     });
 
     test('Should calculate age in Mercury years from earth years', () => {
-        const ageCalc = new AgeCalc(25);
+        // const ageCalc = new AgeCalc(25);
         expect(ageCalc.getMercuryAge()).toEqual(104.17);
     });
 
     test('Should calculate age in Venus years from earth years', () => {
-        const ageCalc = new AgeCalc(25);
+        // const ageCalc = new AgeCalc(25);
         expect(ageCalc.getVenusAge()).toEqual(40.32);
     });
 
     test('Should calculate age in Mars years from earth years', () => {
-        const ageCalc = new AgeCalc(25);
+        // const ageCalc = new AgeCalc(25);
         expect(ageCalc.getMarsAge()).toEqual(13.30);
     });
 
     test('Should calculate age in Jupiter years from earth years', () => {
-        const ageCalc = new AgeCalc(25);
+        // const ageCalc = new AgeCalc(25);
         expect(ageCalc.getJupiterAge()).toEqual(2.11);
     });
 
