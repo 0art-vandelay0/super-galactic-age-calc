62aa77e8eb57491b730142021d0fe0c36e57a14f
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-23 11:11:41 -0700

    BirthdayYears constructor; test - passed

--- src/js/birthdayYears.js
+++ src/js/birthdayYears.js
@@ -1,1 +1,17 @@
 import AgeCalc from './ageCalc.js';
+
+export default class BirthdayYears {
+    constructor(ageCalc, pastBirthday, futureBirthday) {
+        this.mercuryYear = ageCalc.solarPlanetYears.mercury;
+        this.venusYear = ageCalc.solarPlanetYears.venus;
+        this.marsYear = ageCalc.solarPlanetYears.mars;
+        this.jupiterYear = ageCalc.solarPlanetYears.jupiter;
+        this.pastBirthday = pastBirthday;
+        this.futureBirthday = futureBirthday;
+    }
+
+    getMercuryYearsPastBirthday() {
+        const mercuryYearsPastBirthday = this.pastBirthday / this.mercuryYear;
+        return Number(mercuryYearsPastBirthday.toFixed(2));
+    }
+}
