77135f9e1ff9e5bd24073a736723aaf956efc928
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-23 08:33:23 -0700

    AgeCalc test 1; good fail

--- __tests__/ageCalc.test.js
+++ __tests__/ageCalc.test.js
@@ -1,8 +1,8 @@
 import AgeCalc from '../src/js/ageCalc.js';
 
 describe('AgeCalc', () => {
-    Test('Should correctly create an ageCalc object with earthAge property', () => {
+    test('Should correctly create an ageCalc object with earthAge property', () => {
         const ageCalc = new AgeCalc(25);
-        expect(ageCalc.earthAge).toEqual(25);
+        expect(ageCalc.earthAge).toEqual(20);
     });
 });
---
+++ src/js/ageCalc.js
@@ -0,0 +1,5 @@
+export default class AgeCalc {
+    constructor(age) {
+        this.age = age;
+    } 
+}
