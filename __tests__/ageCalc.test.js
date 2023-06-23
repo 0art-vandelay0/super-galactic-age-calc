764af124d2390ec9a63652b3a318096d65099baa
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-23 08:52:56 -0700

    AgeCalc test 2; passed

--- __tests__/ageCalc.test.js
+++ __tests__/ageCalc.test.js
@@ -9,9 +9,9 @@ describe('AgeCalc', () => {
     test('Should correctly create an ageCalc object with solarPlanetYears property', () => {
         const ageCalc = new AgeCalc(25);
         expect(ageCalc.solarPlanetYears).toEqual({
-            mercury: 0.23,
-            venus: 0.61,
-            mars: 1.87,
+            mercury: 0.24,
+            venus: 0.62,
+            mars: 1.88,
             jupiter: 11.86
         });
     });
