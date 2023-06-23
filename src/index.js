f14e91b6a28960736d03b7cca363d23534f01c5b
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-23 15:52:08 -0700

    Add UI logic for AgeCalc and styling

--- src/index.js
+++ src/index.js
@@ -8,12 +8,26 @@ function handleEarthAgeForm(event) {
     document.querySelector('#response').innerText = null;
     const earthAgeInput = document.querySelector('#earth-age-input').value;
     const age = new AgeCalc(earthAgeInput);
-    const response = age.getMercuryAge();
-    const pTag = document.createElement("p");
-    pTag.append(`Your age on Mercury is: ${response}.`);
-    document.querySelector('#response').append(pTag);
+    const mercAge = age.getMercuryAge();
+    const venusAge = age.getVenusAge();
+    const marsAge = age.getMarsAge();
+    const jupiterAge = age.getJupiterAge();
+    const pTag1 = document.createElement("p");
+    const pTag2 = document.createElement("p");
+    const pTag3 = document.createElement("p");
+    const pTag4= document.createElement("p");
+    pTag1.append(`You are ${mercAge} years old on Mercury.`);
+    pTag2.append(`You are  ${venusAge} years old on Venus.`);
+    pTag3.append(`You are  ${marsAge} years old on Mars.`);
+    pTag4.append(`You are  ${jupiterAge} years old on Jupiter.`);
+    document.querySelector('#response').append(pTag1);
+    document.querySelector('#response').append(pTag2);
+    document.querySelector('#response').append(pTag3);
+    document.querySelector('#response').append(pTag4);
+    document.querySelector('#responses-div').removeAttribute("class", "hidden");
 }
 
 window.addEventListener("load", function() {
     document.querySelector("#earthAge-form").addEventListener("submit", handleEarthAgeForm);
+    // document.querySelector("#age-passed-form").addEventListener("submit", handlePastBdayForm);
 });
