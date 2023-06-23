c010901611d8538f10020debd5a7a4d1be08b7ac
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-23 08:50:41 -0700

    AgeCalc test 2; good fail

--- src/js/ageCalc.js
+++ src/js/ageCalc.js
@@ -1,5 +1,11 @@
 export default class AgeCalc {
     constructor(earthAge) {
         this.earthAge = earthAge;
+        this.solarPlanetYears = {
+            mercury: 0.24,
+            venus: 0.62,
+            mars: 1.88,
+            jupiter: 11.86
+        };
     } 
 }
